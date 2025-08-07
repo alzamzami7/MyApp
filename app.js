8a02975587e8d81f289179656df443a6d4ad51ed
alzamzami7 <alzamzamiabdallah@gmail.com>
2025-08-08 02:40:55 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-center mt-10 text-2xl text-blue-600">
+      أهلاً عبدالله 🌟 موقعك يعمل!
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
